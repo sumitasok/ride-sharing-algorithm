b4df9aaae5264d6d16ac2d342478ff1334f78e22
Sumit Asok <sumitasok@gmail.com>
2017-01-14 16:46:51 +0530

    vehicle remaining occupancy

--- vehicle_test.go
+++ vehicle_test.go
@@ -10,9 +10,20 @@ func TestVehicleOccupancy(t *testing.T) {
 	assert := assert.New(t)
 
 	v := NewVehicle(4, location{Lat: 12.223234, Long: 77.23123})
-	v.addRequestor(*NewRequestor("rider-1", 2, time.Now(), location{Lat: 12.223234, Long: 77.23123}, location{Lat: 12.223234, Long: 77.23123}))
+	v.addRequestor(*NewRequestor("rider-1", 1, time.Now(), location{Lat: 12.223234, Long: 77.23123}, location{Lat: 12.223234, Long: 77.23123}))
 
-	assert.Equal(int64(2), v.occupancyStatus())
+	assert.Equal(int64(1), v.occupancyStatus())
+
+	assert.True(true)
+}
+
+func TestVehicleRemainingOccupancy(t *testing.T) {
+	assert := assert.New(t)
+
+	v := NewVehicle(4, location{Lat: 12.223234, Long: 77.23123})
+	v.addRequestor(*NewRequestor("rider-1", 1, time.Now(), location{Lat: 12.223234, Long: 77.23123}, location{Lat: 12.223234, Long: 77.23123}))
+
+	assert.Equal(int64(3), v.remainingOccupancy())
 
 	assert.True(true)
 }
